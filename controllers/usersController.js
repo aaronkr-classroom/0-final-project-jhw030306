0d539f970876a20bfcb2a59137f9380b91857394
jhw030306 <154288326+jhw030306@users.noreply.github.com>
2024-06-21 01:00:15 +0000

    3

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,45 +1,65 @@
 // controllers/usersController.js
 "use strict";
 
-/**
- * Listing 18.9 (p. 268-269)
- * Listing 18.11 (p. 271)
- * userController.js에서 인덱스 액션 생성과 index 액션의 재방문
- */
+const passport = require("passport"); // passport 모듈 요청
 const User = require("../models/User"); // 사용자 모델 요청
 
+const getUserParams = (body) => {
+  return {
+    username: body.username,
+    name: {
+      first: body.first,
+      last: body.last,
+    },
+    email: body.email,
+    password: body.password,
+    profileImg: body.profileImg,
+  };
+};
+
 module.exports = {
+  login: (req, res) => {
+    res.render("users/login", {
+      page: "login",
+      title: "Login",
+    });
+  },
+
+  authenticate: passport.authenticate("local", {
+    failureRedirect: "/users/login",
+    failureFlash: "Failed to login.",
+    successRedirect: "/",
+    successFlash: "Logged in!"
+  }),
+
+  logout: (req, res, next) => {
+    req.logout(() => {
+      console.log("Logged out!");
+    });
+    res.locals.redirect = "/";
+    req.flash("success", "Logged out!");
+    next();
+  },
+
   index: (req, res, next) => {
-    User.find() // index 액션에서만 퀴리 실행
+    User.find()
       .then((users) => {
-        // 사용자 배열로 index 페이지 렌더링
-        res.locals.users = users; // 응답상에서 사용자 데이터를 저장하고 다음 미들웨어 함수 호출
+        res.locals.users = users;
         next();
       })
       .catch((error) => {
-        // 로그 메시지를 출력하고 홈페이지로 리디렉션
         console.log(`Error fetching users: ${error.message}`);
-        next(error); // 에러를 캐치하고 다음 미들웨어로 전달
+        next(error);
       });
   },
+
   indexView: (req, res) => {
     res.render("users/index", {
       page: "users",
       title: "All Users",
-    }); // 분리된 액션으로 뷰 렌더링
+    });
   },
 
-  /**
-   * 노트: 구독자 컨트롤러에서 index 액션이 getAllSubscribers를 대체한다. main.js에서 액션 관련
-   * 라우트 index를 가리키도록 수정하고 subscribers.ejs를 index.ejs로 변경된 점을 기억하자. 이
-   * 뷰는 views 폴더 아래 subscribers 폴더에 있어야 한다.
-   */
-
-  /**
-   * Listing 19.2 (p. 278)
-   * userController.js에 액션 생성 추가
-   */
-  // 폼의 렌더링을 위한 새로운 액션 추가
   new: (req, res) => {
     res.render("users/new", {
       page: "new-user",
@@ -47,7 +67,6 @@ module.exports = {
     });
   },
 
-  // 사용자를 데이터베이스에 저장하기 위한 create 액션 추가
   create: (req, res, next) => {
     let userParams = {
       name: {
@@ -59,7 +78,7 @@ module.exports = {
       password: req.body.password,
       profileImg: req.body.profileImg,
     };
-    // 폼 파라미터로 사용자 생성
+
     User.create(userParams)
       .then((user) => {
         res.locals.redirect = "/users";
@@ -72,58 +91,18 @@ module.exports = {
       });
   },
 
-  // 분리된 redirectView 액션에서 뷰 렌더링
   redirectView: (req, res, next) => {
     let redirectPath = res.locals.redirect;
     if (redirectPath) res.redirect(redirectPath);
     else next();
   },
 
-  /**
-   * 노트: 구독자 컨트롤러에 new와 create 액션을 추가하는 것은 새로운 CRUD 액션을 맞춰
-   * getAllSubscribers와 saveSubscriber 액션을 삭제할 수 있다는 의미다. 게다가 홈
-   * 컨트롤러에서 할 것은 홈페이지인 index.ejs 제공밖에 없다.
-   */
-
-  /**
-   * Listing 19.7 (p. 285)
-   * userController.js에서 특정 사용자에 대한 show 액션 추가
-   */
   show: (req, res, next) => {
-    let userId = req.params.id; // request params로부터 사용자 ID 수집
-    User.findById(userId) // ID로 사용자 찾기
-      .then((user) => {
-        res.locals.user = user; // 응답 객체를 통해 다음 믿들웨어 함수로 사용자 전달
-        next();
-      })
-      .catch((error) => {
-        console.log(`Error fetching user by ID: ${error.message}`);
-        next(error); // 에러를 로깅하고 다음 함수로 전달
-      });
-  },
-
-  // show 뷰의 렌더링
-  showView: (req, res) => {
-    res.render("users/show", {
-      page: "user-details",
-      title: "User Details",
-    });
-  },
-
-  /**
-   * Listing 20.6 (p. 294)
-   * edit와 update 액션 추가
-   */
-  // edit 액션 추가
-  edit: (req, res, next) => {
     let userId = req.params.id;
-    User.findById(userId) // ID로 데이터베이스에서 사용자를 찾기 위한 findById 사용
+    User.findById(userId)
       .then((user) => {
-        res.render("users/edit", {
-          user: user,
-          page: "edit-user",
-          title: "Edit User",
-        }); // 데이터베이스에서 내 특정 사용자를 위한 편집 페이지 렌더링
+        res.locals.user = user;
+        next();
       })
       .catch((error) => {
         console.log(`Error fetching user by ID: ${error.message}`);
@@ -131,7 +110,29 @@ module.exports = {
       });
   },
 
-  // update 액션 추가
+  showView: (req, res) => {
+    res.render("users/show", {
+      page: "user-details",
+      title: "User Details",
+    });
+  },
+
+  edit: (req, res, next) => {
+    let userId = req.params.id;
+    User.findById(userId)
+      .then((user) => {
+        res.render("users/edit", {
+          user: user,
+          page: "edit-user",
+          title: "Edit User",
+        });
+      })
+      .catch((error) => {
+        console.log(`Error fetching user by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
   update: (req, res, next) => {
     let userId = req.params.id,
       userParams = {
@@ -143,15 +144,17 @@ module.exports = {
         username: req.body.username,
         password: req.body.password,
         profileImg: req.body.profileImg,
-      }; // 요청으로부터 사용자 파라미터 취득
+      };
 
-    User.findByIdAndUpdate(userId, {
-      $set: userParams,
-    }) //ID로 사용자를 찾아 단일 명령으로 레코드를 수정하기 위한 findByIdAndUpdate의 사용
+    User.findByIdAndUpdate(userId, userParams, { new: true })
       .then((user) => {
-        res.locals.redirect = `/users/${userId}`;
-        res.locals.user = user;
-        next(); // 지역 변수로서 응답하기 위해 사용자를 추가하고 다음 미들웨어 함수 호출
+        if (user) {
+          res.locals.redirect = `/users/${userId}`;
+          res.locals.user = user;
+          next();
+        } else {
+          next(new Error(`User ID ${userId} not found`));
+        }
       })
       .catch((error) => {
         console.log(`Error updating user by ID: ${error.message}`);
@@ -159,20 +162,20 @@ module.exports = {
       });
   },
 
-  /**
-   * Listing 20.9 (p. 298)
-   * delete 액션의 추가
-   */
   delete: (req, res, next) => {
     let userId = req.params.id;
-    User.findByIdAndRemove(userId) // findByIdAndRemove 메소드를 이용한 사용자 삭제
-      .then(() => {
-        res.locals.redirect = "/users";
-        next();
+    User.findByIdAndRemove(userId)
+      .then((user) => {
+        if (user) {
+          res.locals.redirect = "/users";
+          next();
+        } else {
+          next(new Error(`User ID ${userId} not found`));
+        }
       })
       .catch((error) => {
         console.log(`Error deleting user by ID: ${error.message}`);
-        next();
+        next(error);
       });
   },
 };
