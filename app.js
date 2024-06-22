dc04971044d794d4c8c8467e72019b87842e3e15
jhw030306 <154288326+jhw030306@users.noreply.github.com>
2024-06-22 17:05:01 +0000

    finish

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ db.once("opne", () => {
  * =====================================================================
  */
 
-app.set("port", process.env.PORT || 3010);
+app.set("port", process.env.PORT || 3000);
 
 // ejs 레이아웃 렌더링
 app.set("view engine", "ejs"); // ejs를 사용하기 위한 애플리케이션 세팅
